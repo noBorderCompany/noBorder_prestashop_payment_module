f4ed00f82dbf92b878b0a684498a544fc76cabf7
noBorderTech <152797841+noBorderTech@users.noreply.github.com>
2023-12-03 19:01:32 +0330

    Add files via upload

---
+++ noborder.php
@@ -0,0 +1,146 @@
+<?php
+
+/*
+* noborder payment gateway
+* @developer Hanif Zekri
+* @publisher noborder
+* @copyright (C) 2020 noborder
+* @version  1.1
+* @license http://www.gnu.org/licenses/gpl-2.0.html GPLv2 or later
+* https://noborder.tech
+*/
+
+if (!defined('_PS_VERSION_')) exit;
+
+class noborder extends PaymentModule {
+	
+	private $_html = '';
+    private $_postErrors = array();
+	public $address;
+	
+	public function __construct(){
+		$this->name = 'noborder';
+        $this->tab = 'payments_gateways';
+        $this->version = '1.1';
+        $this->author = 'noborder';
+        $this->controllers = array('payment', 'validation');
+        $this->currencies = true;
+        $this->currencies_mode = 'checkbox';
+        $this->bootstrap = true;
+        $this->displayName = 'noborder';
+        $this->description = 'noborder Crypto payment gateway';
+        $this->confirmUninstall = 'Are you sure ?';
+        $this->ps_versions_compliancy = array('min' => '1.7.0', 'max' => _PS_VERSION_);
+        $this->bootstrap = true;
+        parent::__construct();
+    }
+	
+	public function install(){
+		return parent::install() && $this->registerHook('paymentOptions') && $this->registerHook('paymentReturn');
+    }
+	
+	public function uninstall(){
+		return parent::uninstall();
+    }
+
+    public function getContent(){
+		
+		if (Tools::isSubmit('noborder_submit')) {
+            Configuration::updateValue('noborder_api_key', noborder::sanitize($_POST['noborder_api_key']));
+            Configuration::updateValue('noborder_pay_currency', noborder::sanitize($_POST['noborder_pay_currency']));
+            Configuration::updateValue('noborder_success_massage', noborder::sanitize($_POST['noborder_success_massage']));
+            Configuration::updateValue('noborder_failed_massage', noborder::sanitize($_POST['noborder_failed_massage']));
+            $this->_html .= '<div class="conf confirm">' . $this->l('Settings updated') . '</div>';
+        }
+
+        $this->_generateForm();
+        return $this->_html;
+
+    }
+
+    public static function sanitize($variable){
+        return trim(strip_tags($variable));
+    }
+
+    private function _generateForm(){
+        $this->_html .= '
+		
+		<form action="' . $_SERVER['REQUEST_URI'] . '" method="post" class="defaultForm form-horizontal">
+		<div class="panel">
+		<div class="form-wrapper">
+		
+		<div class="form-group">
+		<label class="control-label col-lg-4 required"> API Key : </label>
+		<div class="col-lg-8">
+		<input type="text" name="noborder_api_key" value="' . Configuration::get('noborder_api_key') . '">
+		<div class="help-block"> You can create an API Key by going to <a href="https://noborder.tech/cryptosite" target="_blank">https://noborder.tech/cryptosite</a></div>
+		</div>
+		</div>
+		
+		<div class="form-group">
+        <label class="control-label col-lg-4 required"> Pay Currencies : </label>
+		<div class="col-lg-8">
+		<input type="text" name="noborder_pay_currency" value="' . Configuration::get('noborder_pay_currency') . '">
+		<div class="help-block"> By default, customers can pay through all <a href="https://noborder.tech/cryptosite" target="_blank">active currencies</a> in the gate, but if you want to limit the customer to pay through one or more specific crypto currencies, you can declare the name of the crypto currencies through this variable. If you want to declare more than one currency, separate them with a dash ( - ). </div>
+		</div>
+		</div>
+		
+		<div class="form-group">
+        <label class="control-label col-lg-4 required"> Success message : </label>
+		<div class="col-lg-8">
+		<textarea dir="auto" name="noborder_success_massage" style="height: 100px;">' . (!empty(Configuration::get('noborder_success_massage')) ? Configuration::get('noborder_success_massage') : "پرداختYour payment has been successfully completed. <br><br> Cart id : {cart_id} <br> Track id: {request_id}") . '</textarea>
+		<div class="help-block">Enter the message you want to display to the customer after a successful payment. You can also choose these placeholders {request_id}, {cart_id} for showing the cart id and the tracking id respectively.</div>
+		</div>
+		</div>
+		
+		<div class="form-group">
+        <label class="control-label col-lg-4 required"> Failure message : </label>
+		<div class="col-lg-8">
+		<textarea dir="auto" name="noborder_failed_massage" style="height: 100px;">' . (!empty(Configuration::get('noborder_failed_massage')) ? Configuration::get('noborder_failed_massage') : "Your payment has failed. Please try again or contact the site administrator in case of a problem. <br><br> Cart id : {cart_id} <br> Track id: {request_id}") . '</textarea>
+		<div class="help-block"> Enter the message you want to display to the customer after a failure occurred in payment. You can also choose these placeholders {request_id}, {cart_id} for showing the cart id and the tracking id respectively. </div>
+		</div>
+		</div>
+		</div>
+		
+		<div class="panel-footer">
+		<input type="submit" name="noborder_submit" value="' . $this->l('Save') . '" class="btn btn-default pull-right">
+		</div>
+		
+		</div>
+		</form>';
+    }
+
+    public function hookPaymentOptions($params){
+	
+		if (!$this->active) return;
+		
+		//form data will be sent to validation controller when user finishes order process
+		$formAction = $this->context->link->getModuleLink($this->name, 'validation', array(), true);
+		
+		//Assign the url form action to the template var $action
+		$this->smarty->assign(['action' => $formAction]);
+		
+		//Load form template to be displayed in the checkout step
+		$paymentForm = $this->fetch('module:noborder/views/templates/hook/payment_options.tpl');
+		
+		//Create a PaymentOption object to display module in checkout
+		$displayName = 'noborder Crypto payment gateway';
+        $newOption = new PrestaShop\PrestaShop\Core\Payment\PaymentOption;
+        $newOption->setModuleName($this->displayName)->setCallToActionText($displayName)->setAction($formAction)->setForm($paymentForm);
+		$payment_options = array($newOption);
+		return $payment_options;
+    }
+	
+	public function hookPaymentReturn($params){
+		if (!$this->active) return;
+		return $this->fetch('module:noborder/views/templates/hook/payment_return.tpl');
+    }
+	
+	public function hash_key(){
+        $en = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+        $one = rand(1, 26);
+        $two = rand(1, 26);
+        $three = rand(1, 26);
+        return $hash = $en[$one] . rand(0, 9) . rand(0, 9) . $en[$two] . $en[$three] . rand(0, 9) . rand(10, 99);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,99 @@
+# noBorder Crypto Payment Gateway for Prestashop
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Configuration](#configuration)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Troubleshooting](#troubleshooting)
+- [License](#license)
+- [Contact Information](#contact-information)
+- [Changelog](#changelog)
+
+---
+
+## Description
+
+The noBorder Crypto Payment Gateway for Prestashop is a plugin that integrates the noBorder cryptocurrency payment gateway into your Prestashop e-commerce website. With this plugin, you can accept cryptocurrency payments from your customers, offering them an additional payment option.
+
+- **Plugin Name:** noBorder Crypto Payment Gateway for PrestaShop
+- **Version:** 1.1
+- **Author:** noBorder.tech
+- **Author URI:** [https://noBorder.tech](https://noBorder.tech)
+- **Author Email:** info@noBorder.tech
+- **Text Domain:** noBorder_prestashop_payment_module
+- **WC tested up to:** 8.1
+- **Copyright (C):** 2020 noBorder
+- **License:** [GPLv3 or later](http://www.gnu.org/licenses/gpl-3.0.html)
+
+---
+
+## Installation
+
+1. Upload the `noBorder` folder to the `modules` directory of your Prestashop installation.
+2. Log in to your Prestashop admin panel.
+3. Navigate to the "Modules and Services" section.
+4. Search for "noBorder" in the modules list and click the "Install" button.
+
+---
+
+## Configuration
+
+1. After installation, go to the "Modules and Services" section in your Prestashop admin panel.
+2. Search for "noBorder" and click on it to configure the settings.
+3. Configure the following options:
+   - API Key: Enter your noBorder API key.
+   - Payable Currencies: Specify the currencies you want to accept for payments. You can enter multiple currencies separated by dashes (e.g., BTC-ETH-LTC).
+   - Successful Payment Message: Customize the message displayed to customers after a successful payment. You can use placeholders like `{cart_id}` and `{request_id}` to display order-specific information.
+   - Failed Payment Message: Customize the message displayed to customers if a payment fails. You can use placeholders like `{cart_id}` and `{request_id}` to display order-specific information.
+4. Click the "Save" button to save your settings.
+
+---
+
+## Usage
+
+Once the noBorder plugin is installed and configured, customers will see "پرداخت رمز ارزی با دیجی درگاه" (Pay with noBorder) as a payment option during the checkout process. They can select this option to complete their purchase using cryptocurrencies.
+
+---
+
+## Contributing
+
+If you want to contribute to this project or report issues, please visit the GitHub repository: [noBorder Crypto Payment Gateway for PrestaShop](https://github.com/noBorder/prestashop_payment_module).
+
+---
+
+## Troubleshooting
+
+If you encounter any issues or have questions about the noBorder Crypto Payment Gateway for PrestaShop, please refer to the [official documentation](https://noBorder.tech) or contact our support team at [info@noBorder.tech](mailto:info@noBorder.tech).
+
+---
+
+## License
+
+This project is licensed under the GPLv3 or later. See the [GNU General Public License](http://www.gnu.org/licenses/gpl-3.0.html) for more details.
+
+---
+
+## Contact Information
+
+If you have any questions or need assistance, please contact us at [info@noBorder.tech](mailto:info@noBorder.tech).
+
+---
+
+## Changelog
+
+- 1.1, feb 18, 2023
+Bugs fixed
+
+- 1.0, jan 15, 2023
+First release.
+
+---
+
+Thank you for using the noBorder Crypto Payment Gateway for PrestaShop! We appreciate your business.
+
+---
+
+*Copyright (C) 2020 noBorder*
